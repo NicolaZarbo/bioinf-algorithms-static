be73d6acc293e0f81dd8667af0e7be48e8e88a7c
NicolaZarbo <nicola.zarbo@mail.polimi.it>
2022-07-21 19:56:32 +0200

    updated bruijn

--- app_bio/bruijn_stuff/brujin.js
+++ app_bio/bruijn_stuff/brujin.js
@@ -1,4 +1,5 @@
 function graphCreator(){
+    directional=(document.getElementById("isNonDirectional").checked);
     let rows=document.getElementById("rows").value;
     let columns=document.getElementById("columns").value;
 
@@ -14,7 +15,7 @@ function graphCreator(){
             node.className="node";
         
             node.setAttribute( "onclick","pointerBase("+k+");");
-            node.innerText=k;
+            node.innerHTML="<div style='z-index: 100;position: relative; pointer-events: none;' >"+k+"</div>";
             zone.appendChild(node);
             graph[k]=[];
             k++;
@@ -48,13 +49,21 @@ function savePosition(event){
         pos1=0;
     }
 }
+/**
+ * saves the clicked key as based if there isn't already on, in that case inserts in the graph the edge between the 2 nodes
+ * @param {int} key key of the clicked node
+ */
 function pointerBase(key){
-    
     if(based!=-1){
         if(!graph[based].includes(key)){
             const basedArray=graph[based];
             let dim=basedArray.length;
             graph[based][dim]=key;
+            if(!directional){
+                const otherNodeArray=graph[key];
+                let dimm=otherNodeArray.length;
+                graph[key][dimm]=based;
+            }
         }
         based=-1;
     }
@@ -94,14 +103,15 @@ function drawArrow(){
     let basePositionx=baseCordinates[0]-(width/2)-vec[0]/2;
     if(vec[0]>0)
         {basePositionx=endCordinates[0]-(width/2)+vec[0]/2;}
-     let arr='<div class="arrowContainer" style="left:'+(basePositionx) +'px; top:'+(baseCordinates[1]-vec[1]/2)+'px; transform:rotateZ('+(angle)+'rad) ">';
-     arr+='<div style="width:'+width+'px" class="stick"></div><div style="right:'+arrowMove+'px;" class="arrow"></div>   </div>';
-     document.getElementById("brate").innerHTML=arr+document.getElementById("brate").innerHTML;
-     baseCordinates[0]=0;
-     baseCordinates[1]=0;
-     based=-1;
-     writeInfo();
-     
+
+    let invisibleArrow=(directional)? "":"visibility:hidden;";
+    let arr='<div class="arrowContainer" style="left:'+(basePositionx) +'px; top:'+(baseCordinates[1]-vec[1]/2)+'px; transform:rotateZ('+(angle)+'rad) ">';
+    arr+='<div style="width:'+width+'px" class="stick"></div><div style="right:'+arrowMove+'px;'+invisibleArrow+'" class="arrow"></div>   </div>';
+    document.getElementById("brate").innerHTML=arr+document.getElementById("brate").innerHTML;
+    baseCordinates[0]=0;
+    baseCordinates[1]=0;
+    based=-1;
+    writeInfo();     
 }
 function findAngle(vector){
     let modulesqr= Math.sqrt(vector[0]*vector[0]+vector[1]*vector[1]);
@@ -115,33 +125,70 @@ function findAngle(vector){
     }
     return angle;
 }
+let hasPath;
 function writeInfo(){
     const info=document.getElementById("graphInfo");
     info.textContent="";
+    hasPath=false;
     if(graphHasPath()){
-        info.textContent=" EULERIAN PATH      ";
+        info.textContent=" EULERIAN PATH  ";
+        hasPath=true;
     }
     if(graphHasCircle()){
+        hasPath=true;
+
         info.textContent="EULERIAN CIRCLE ";
     }
 }
+let directional;
+let starterNode;
+
 function graphHasPath(){
-    let oddOnes=0;
-    for (let i = 0; i < graph.length; i++) {
-        const nOfEdges=numberOfIncomingEdges(i)+numberOfOutgoingEdges(i);
-        if(nOfEdges%2==1){
-            oddOnes++;
+    //directional=!document.getElementById("isNonDirectional").value;
+    if(directional){
+        let negativeOut=0;
+        let positive =0
+        for (let i = 0; i < graph.length; i++) {
+            const deltaEdges=numberOfIncomingEdges(i)-numberOfOutgoingEdges(i);
+            if(deltaEdges==1){
+                positive++;
+                starterNode=i;
+            }
+            if(deltaEdges==-1){
+                negativeOut++;
+            }
         }
+        return positive==negativeOut && positive==1;
+    }else{
+        let oddOnes=0;
+        for (let i = 0; i < graph.length; i++) {
+            const nOfEdges=(numberOfIncomingEdges(i)+numberOfOutgoingEdges(i))/2;
+            if((nOfEdges)%2==1){
+                oddOnes++;
+                starterNode=i;
+            }
+        }
+        return oddOnes<=2;
     }
-    return oddOnes<=2;
 }
 function graphHasCircle(){
-    for (let i = 0; i < graph.length; i++) {
-        if(numberOfIncomingEdges(i)!=numberOfOutgoingEdges(i)){
-            return false;
+    if(!directional){
+        for (let i = 0; i < graph.length; i++) {
+            const nOfEdges=(numberOfIncomingEdges(i)+numberOfOutgoingEdges(i))/2;
+            if(nOfEdges%2==1){
+                return false;
+            }
         }
+        return true;
+
+    }else{
+        for (let i = 0; i < graph.length; i++) {
+            if(numberOfIncomingEdges(i)!=numberOfOutgoingEdges(i)){
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
 }
 function numberOfIncomingEdges(nodeKey){
     let sum=0;
@@ -157,8 +204,13 @@ function numberOfOutgoingEdges(nodeKey){
     return graph[nodeKey].length;
 }
 function printPath(){
+    writeInfo();
     findEulerianPath();
     const zone=document.getElementById("pathZone");
+    if(!hasPath){
+        zone.innerText="there is no path";
+        return;
+    }
     zone.innerText="";
     for (let i = ePath.length-1; i >=0 ; i--) {
         let bro=document.createElement("div");
@@ -167,7 +219,6 @@ function printPath(){
         bro.style="pointer-events: none; "
         zone.appendChild(bro);
         if(i>0){zone.innerHTML+='<div class="arrowContainer" style="position:relative"><div  class="stick"></div><div style="right: -65px;" class="arrow"></div>   </div>';}
-        
     }
 }
 
@@ -179,17 +230,20 @@ function findEulerianPath(){
     ePath.length=0;
     visitedEdges.length=0;
     tempPath.length=0;
+    if(!hasPath){
+        return;
+    }
     for (let i = 0; i < graph.length; i++) {
         visitedEdges[i]=[];
     }
-    let nodeDim=[];
-    for (let i = 0; i < graph.length; i++) {
-        const node = graph[i]
-        nodeDim[i] =numberOfOutgoingEdges(i);
-    }
+   // let nodeDim=[];
+    //for (let i = 0; i < graph.length; i++) {
+      //  const node = graph[i]
+        //nodeDim[i] =numberOfOutgoingEdges(i);
+    //}
     //const nodeDim=graph.map( node=> {numberOfIncomingEdges(node)+numberOfOutgoingEdges(node);});
-    const max= maxOfArray(nodeDim);
-    tempPath[0]=nodeDim.indexOf(max);
+    //const max= maxOfArray(nodeDim);
+    tempPath[0]=starterNode;
     popper(tempPath[0]);
 
 }
@@ -221,9 +275,18 @@ function popper(top){
         popper(tempPath[tempPath.length-1]);
     }
 }
+/**
+ * 
+ * @param {int} node the key of the node
+ * @param {int} edge the key of the other node
+ */
 function addToVisited(node, edge){
     visitedEdges[node][visitedEdges[node].length]=edge;
+    if(!directional){
+        visitedEdges[edge][visitedEdges[edge].length]=node;
+    }
 }
+
 function availableEdges(node){
     const edges=outgoingEdges(node);
     let freeEdges=[];
@@ -236,6 +299,7 @@ function availableEdges(node){
     });
     return freeEdges;
 }
+
 function outgoingEdges(node){
     return graph[node];
 }
